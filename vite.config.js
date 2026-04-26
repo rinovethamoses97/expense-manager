55a0ab1418c5f7e03ed482e0e0e73b9a488b3889
Rino <rinovethamoses97@gmail.com>
2026-04-26 12:49:17 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api1': { target: 'http://localhost:5000', changeOrigin: true },
-      '/authe': { target: 'http://localhost:5000', changeOrigin: true },
+      '/api': { target: 'http://localhost:5000', changeOrigin: true },
+      '/auth': { target: 'http://localhost:5000', changeOrigin: true },
     },
   },
 });
